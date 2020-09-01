14ac07a1e98e016b5ad5239e1e141e6c4eae01ba
Alex Lynch <mlynch5187@gmail.com>
2020-09-01 10:29:13 -0600

    Fix typos to correct json response

--- app/poros/member.rb
+++ app/poros/member.rb
@@ -1,23 +1,23 @@
 class Member
 
-  def house_members(house)
-    response = Faraday.get('https://www.potterapi.com/v/characters') do |f|
-      f.params['key'] = ENV['POTTER_KEY']
+  def self.house_members(house)
+    response = Faraday.get('https://www.potterapi.com/v1/characters') do |f|
+      f.params['key'] = '$2a$10$2klzYEw2EcBFCX6UW6actOfDX7p8laQc9R0S6XBcyIkyfA3WLmwXG'
       f.params['house'] = house
-      f.params['OrderOfThePhoenix'] = true
+      f.params['orderOfThePhoenix'] = true
     end
-    members = JSON.parse(responde.body, symbolize_names: true)
-    members.map do |member|
-      Member.new(member)
+    members = JSON.parse(response.body, symbolize_names: true)
+    members.map do |member_data|
+      Member.new(member_data)
     end
   end
 
   attr_reader :name, :house, :role, :patronus
 
-  def initialize(member)
-    @name = member[:name]
-    @house = member[:house]
-    @role = member[:role]
-    @patronus = member[:patronus]
+  def initialize(member_data)
+    @name = member_data[:name]
+    @house = member_data[:house]
+    @role = member_data[:role]
+    @patronus = member_data[:patronus]
   end
 end
