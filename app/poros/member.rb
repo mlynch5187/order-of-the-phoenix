3589c0388e2c40455f16e757baeb4c21647f195a
Alex Lynch <mlynch5187@gmail.com>
2020-09-01 10:04:39 -0600

    Create house_members method in member PORO

--- app/poros/member.rb
+++ app/poros/member.rb
@@ -1,3 +1,11 @@
 class Member
 
+  def house_members(house)
+    response = Faraday.get('https://www.potterapi.com/v/characters') do |f|
+      f.params['key'] = ENV['POTTER_KEY']
+      f.params['house'] = house
+      f.params['OrderOfThePhoenix'] = true
+    end
+    member_info = JSON.parse(responde.body, symbolize_names: true)
+  end
 end
